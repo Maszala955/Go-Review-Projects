1124b1fb7644d2539f065d9b80ced51c3e85521d
Joe Lapert <lapertjoseph@gmail.com>
2026-06-05 14:08:21 +0200

    add new handler : Marshal Encode

--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"net/http"
+	"errors"
 	"io"
 	"log"
+	"net/http"
+	"encoding/json"
 )
 
 // Deux concept clés :
@@ -13,7 +15,7 @@ import (
 // - `r` : la requête entrante (méthode, URL, headers...)
 
 // 2. http.ListenAndServe - démarre le serveur sur un port
-// Exercice : 
+// Exercice :
 /*
 Dans un nouveau fichier server.go :
 Crée un handler pour la route `/hello` qui répond `"Hello, World!"`
@@ -29,11 +31,41 @@ func StartServer() {
 	h2 := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "Bienvenue sur mon serveur")
 	}
+
+	h3 := func(w http.ResponseWriter, r *http.Request) {
+		user, _ := NewUser("test", "test", 12)
+		w.Header().Set("Content-Type", "application/json")
+
+		jsonStr, err := json.Marshal(user)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(jsonStr)
+
+	}
 	
 	http.HandleFunc("/hello", h1)
 	http.HandleFunc("/", h2)
+	http.HandleFunc("/newUser", h3)
 	
 	log.Println("Server listen port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+}
+
+type User struct {
+	Name 	string  `json:"name"`
+	Email string  `json:"email"`
+	Age 	int 		`json:"age"`
+}
+
+func NewUser(name, email string, age int) (User, error) {
+	if (age < 10) {
+		return User{}, errors.New("student too young")
+	} else {
+		return User{Name: name, Email: email, Age: age}, nil
+	}
 }
