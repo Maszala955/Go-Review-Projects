7c1d8bf3ef0a1edc14f812ab2e376fbc5397a678
Joe Lapert <lapertjoseph@gmail.com>
2026-06-09 11:57:29 +0200

    add postgres for consistency and CreateTable and InsertUser functions

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	
+)
+
 
 func main() {
-	StartServer()
+	conn, err := ConnectDB()
+	if err != nil {
+		fmt.Println("Erreur de connexion", err)
+	}
+	fmt.Println("Connecté à PostgreSQL")
+
+	err = CreateTable(conn)
+	if err != nil {
+		fmt.Println("Erreur de connexion", err)
+	}
+	
+	user := User{Name: "Test", Email: "Test@email.com", Age: 40}
+	err = InsertUser(conn, user)
+	if err != nil {
+		fmt.Println("Erreur d'insert", err)
+	}
+	// StartServer()
 }
